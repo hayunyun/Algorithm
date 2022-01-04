3410bc9f92058dd2feb5a4bf4593a53c358eb8dd
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-01-04 23:16:29 +0900

    [Bronze III] Title: 기찍-N, Time: 316 ms, Memory: 21040 KB -BaekjoonHub

---
+++ 기찍-N/기찍-N.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException; 
+import java.util.StringTokenizer;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));   
+        StringTokenizer st; // 여러개를 입력받아 사용할 때
+        
+        int N = Integer.parseInt(br.readLine()); //Int
+
+        for (int i=N; i>=1; i--) {
+            bw.write(String.valueOf(i+"\n"));
+        }
+        bw.flush();
+    }
+}
