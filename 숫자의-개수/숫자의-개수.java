e9e0f2c783e483760c4015a5d7249fa017e7520d
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-01-05 17:10:53 +0900

    Update 숫자의-개수.java

--- 숫자의-개수/숫자의-개수.java
+++ 숫자의-개수/숫자의-개수.java
@@ -20,38 +20,7 @@ public class Main
         int[] nums = new int[10];
         Arrays.fill(nums, 0);
         for (int i=0; i<result.length(); i++) {
-            switch(result.charAt(i)-'0') {
-                case 0:
-                    nums[0] += 1;
-                    break;
-                case 1:
-                    nums[1] += 1;
-                    break;
-                case 2:
-                    nums[2] += 1;
-                    break;
-                case 3:
-                    nums[3] += 1;
-                    break;
-                case 4:
-                    nums[4] += 1;
-                    break;
-                case 5:
-                    nums[5] += 1;
-                    break;
-                case 6:
-                    nums[6] += 1;
-                    break;
-                case 7:
-                    nums[7] += 1;
-                    break;
-                case 8:
-                    nums[8] += 1;
-                    break;
-                case 9:
-                    nums[9] += 1;
-                    break;
-            }
+            nums[result.charAt(i)-'0']++;
         }
         for (int i=0; i<nums.length; i++){
             bw.write(nums[i] + "\n");
