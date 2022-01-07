a15499212b9818b810a6b7017a8fd10c465ef620
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-01-07 21:36:34 +0900

    [Silver V] Title: 그룹-단어-체커, Time: 124 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 그룹-단어-체커/그룹-단어-체커.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));  
+        int N = Integer.parseInt(br.readLine());
+        int cnt = 0; // 그룹 단어 개수
+        
+        for (int i = 0; i < N; i++) {
+            String S = br.readLine();
+            boolean check[] = new boolean[26]; // 알파벳 사용 내역
+            boolean tmp = true; // 그룹 단어 인지
+            
+            for (int j = 0; j < S.length(); j++) {
+                int index = S.charAt(j)-'a';
+                if(check[index]) { // 이전에 사용한적이 있는 문자라면
+                    if(S.charAt(j) != S.charAt(j-1)) { // 이전 문자와 연속되지 않는다면
+                        tmp = false; // 그룹 단어가 아님
+                        break;
+                    }
+                }else { // 이전에 사용한적이 없는 문자라면
+                    check[index] = true; // 문자 사용 체크
+                }
+            }
+            if(tmp) cnt++;
+        }
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+    }
+}
