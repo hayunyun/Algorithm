64f8704387d8dd1fe65b0bdbd1e81f5c5aaa08a0
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-01-09 22:17:18 +0900

    [Bronze III] Title: 팩토리얼, Time: 152 ms, Memory: 15832 KB -BaekjoonHub

--- 팩토리얼/팩토리얼.java
+++ 팩토리얼/팩토리얼.java
@@ -2,19 +2,15 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    public static int fac(int N) {
+        if (N<=1) return 1;
+        return N*fac(N-1);
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));  
         int N = Integer.parseInt(br.readLine());
-        int ans = 1;
-        int cnt = 1;
-        while (N != 0) {
-            ans *= cnt; 
-            cnt++;
-            if (cnt>N)
-                break;
-        }
-        bw.write(ans + "\n");
+        bw.write(fac(N) + "\n");
         bw.flush();
     }
 }
