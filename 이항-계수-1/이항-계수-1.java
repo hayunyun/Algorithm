a5646a702fcdf0d275711eb695cc4ee45d47f25d
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-01-16 22:45:21 +0900

    [Bronze I] Title: 이항-계수-1, Time: 144 ms, Memory: 15824 KB -BaekjoonHub

---
+++ 이항-계수-1/이항-계수-1.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException; 
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));  
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int ans = 1, up = 1;
+        for (int i=K; i>0; i--) {
+            up *= N;
+            ans *= i;
+            N--;
+        }
+        bw.write(up/ans + "\n");
+        bw.flush();
+    }
+}
