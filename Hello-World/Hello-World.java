80495a748ced52f3e319f0ca0f72faa1103016ba
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-01-17 14:09:35 +0900

    백준허브 업데이트

--- Hello-World/Hello-World.java
+++
@@ -1,9 +0,0 @@
-// tip: each public class is put in its own file
-public class Main
-{
-    // tip: arguments are passed via the field below this editor
-    public static void main(String[] args)
-    {
-        System.out.println("Hello World!");
-    }
-}
