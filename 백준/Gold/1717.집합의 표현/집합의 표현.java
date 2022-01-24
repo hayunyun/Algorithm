566b0c3e047f2a8b16b026ad7356197284f96bb8
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-01-24 11:17:06 +0900

    [Gold IV] Title: 집합의 표현, Time: 752 ms, Memory: 59940 KB -BaekjoonHub

---
+++ 백준/Gold/1717.집합의 표현/집합의 표현.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));  
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N+1];
+        // 초기화
+        for (int i = 0; i <= N; i++) {
+            arr[i] = i;
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
+            if (x==1) { // find
+                int aRoot = find(a);
+                int bRoot = find(b);
+                if (aRoot == bRoot)
+                    bw.write("YES\n");
+                else bw.write("NO\n");
+            }
+            else { // union
+                union(a, b);
+            }
+        }
+        bw.flush();
+    }
+
+    static void union(int a, int b) {
+        int aRoot = find(a);
+        int bRoot = find(b);
+        arr[aRoot] = bRoot;
+    }
+
+    static int find(int a) {
+        if (arr[a] == a) return a;
+        else {
+            arr[a] = find(arr[a]);
+            return arr[a];
+        }
+    }
+}
