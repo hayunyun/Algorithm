a37ac0d6789b1a496ecba20422118a133a978c7a
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-01-27 23:12:56 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 136 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/2522.별 찍기 － 12/별 찍기 － 12.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));  
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        
+        int n = Integer.parseInt(br.readLine());
+        
+        for (int i = 1; i <= 2*n-1; i++) {
+            if (i<=n)
+                bw.write(" ".repeat(n-i) + "*".repeat(i));
+            else
+                bw.write(" ".repeat(-n+i) + "*".repeat(2*n-i));
+            bw.write("\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
