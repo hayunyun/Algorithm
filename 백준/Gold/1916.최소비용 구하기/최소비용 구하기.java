51a7c3e3ea17f1d60eb7e80e3df1e84285cf91b2
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-02-16 00:16:39 +0900

    [Gold V] Title: 최소비용 구하기, Time: 584 ms, Memory: 51140 KB -BaekjoonHub

--- 백준/Gold/1916.최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916.최소비용 구하기/최소비용 구하기.java
@@ -23,7 +23,7 @@ public class Main {
         }
     }
 
-    static int N, M;
+    static int N, M, start, end;
     static int[] dist;
     static ArrayList<Edge>[] edges;
     static StringBuilder sb = new StringBuilder();
@@ -31,14 +31,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
         StringTokenizer st;
 
-        // 인접 리스트
-        edges = new ArrayList[N+1];
-        dist = new int[N+1];
+        edges = new ArrayList[N+1]; // 인접리스트 (to, weight) - 해당 인덱스에서 to 까지 가는 데 weight 만큼의 비용이 듦
+        dist = new int[N+1]; // 시작점에서 해당 인덱스 정점까지의 거리
 
         for (int i = 1; i <= N; i++) {
             edges[i] = new ArrayList<>();
@@ -53,13 +51,26 @@ public class Main {
         }
 
         st = new StringTokenizer(br.readLine());
-        int start = Integer.parseInt(st.nextToken());
-        int end = Integer.parseInt(st.nextToken());
-        
-//        dijkstra(start);
+        start = Integer.parseInt(st.nextToken());
+        end = Integer.parseInt(st.nextToken());
+
+        dijkstra(start);
+
+        // 값 확인
+//        for (int i = 1; i <= N; i++) {
+//            if (dist[i] == Integer.MAX_VALUE) sb.append("INF\n");
+//            else sb.append(dist[i]).append('\n');
+//        }
+//        System.out.println(sb);
+        System.out.print(dist[end]);
+    }
+
+    static void dijkstra(int start) {
+        // 모든 정점까지에 대한 거리를 무한대로 초기화
         for (int i = 1; i <= N ; i++) {
             dist[i] = Integer.MAX_VALUE;
         }
+
         // 최소 힙 생성
         PriorityQueue<Info> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o.dist));
 
@@ -71,21 +82,14 @@ public class Main {
         while (!pq.isEmpty()) {
             Info info = pq.poll();
             // 현재 꺼낸 값이 가치있는가?
-            // 기록된 거리보다 현재 꺼낸 값의 거리가 더 크다면 갱신할 필요 X
+            // -> 기록된 거리보다 현재 꺼낸 값의 거리가 더 크다면 갱신할 필요 X
             if (dist[info.index] < info.dist) continue;
             for (Edge e: edges[info.index]) { // 인접한 간선들 하나씩 확인
-                if (dist[info.index] + e.weight >= dist[e.to]) continue; // 만약 새롭게 찾은 거리가 원래 거리보다 크다면 갱신
+                if (dist[info.index] + e.weight >= dist[e.to]) continue; // 만약 새롭게 찾은 거리가 원래 거리보다 크다면 갱신 X
                 // 아니라면
                 dist[e.to] = dist[info.index] + e.weight; // 갱신
                 pq.add(new Info(e.to, dist[e.to])); // 새로운 정보 pq에 넣어주기
             }
         }
-
-        for (int i = 1; i <= N; i++) {
-            if (dist[i] == Integer.MAX_VALUE) sb.append("INF\n");
-            else sb.append(dist[i]).append('\n');
-        }
-//        System.out.println(sb);
-        System.out.print(dist[end]);
     }
 }
