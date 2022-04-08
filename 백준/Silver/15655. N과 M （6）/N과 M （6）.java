dce2aa7942f691a5d39821efd31b2dd372187c22
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-04-08 18:26:08 +0900

    [Silver III] Title: N과 M (6), Time: 128 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static boolean[] check;
+    static int[] arr;
+    static int n, m;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        int[] nums = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
+
+        check = new boolean[n];
+        arr = new int[m];
+
+        backTracking(0, nums);
+        System.out.println(sb);
+    }
+
+    public static void backTracking(int cnt, int[] nums) {
+        if (cnt == m) {
+            for (int val : arr) {
+                sb.append(val).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (cnt == 0 || arr[cnt - 1] < nums[i]) {
+                arr[cnt] = nums[i];
+                backTracking(cnt + 1, nums);
+            }
+        }
+    }
+}
