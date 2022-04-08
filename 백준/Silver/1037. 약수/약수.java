87351d583fd0e40c58a9239c224471b9b54fd721
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-04-08 23:17:50 +0900

    [Silver V] Title: 약수, Time: 124 ms, Memory: 14340 KB -BaekjoonHub

---
+++ 백준/Silver/1037. 약수/약수.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] nums = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+
+        int ans = nums[0] * nums[nums.length - 1];
+        System.out.println(ans);
+    }
+}
