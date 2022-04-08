1b8e5b5b42e21d4078894454df228f5dca47dc84
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-04-09 03:01:56 +0900

    [Silver III] Title: 바이러스, Time: 128 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -2,14 +2,18 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static StringBuilder sb = new StringBuilder();
     static boolean[] check;
     static ArrayList<Integer> [] graph;
-    static int n, m;
+    static int[] arr;
+    static int n, m, value;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         m = Integer.parseInt(br.readLine());
+
         graph = new ArrayList[n + 1];
+        arr = new int [n];
         check = new boolean[n + 1];
 
         for (int i = 0; i < n + 1; i++) {
@@ -23,26 +27,18 @@ public class Main {
             graph[from].add(to);
             graph[to].add(from);
         }
-
-        System.out.println(bfs(1) - 1);
+        value = 0;
+        dfs(1);
+        System.out.println(value - 1);
     }
 
-    public static int bfs(int start) {
-        int ans = 0;
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
-
-        while (!q.isEmpty()) {
-            int now = q.poll();
-            if (!check[now]) {
-                check[now] = true;
-                for(int val : graph[now]) {
-                    q.add(val);
-                }
-                ans++;
+    public static void dfs(int start) {
+        if (!check[start]) {
+            check[start] = true;
+            for (int i = 0; i < graph[start].size(); i++) {
+                dfs(graph[start].get(i));
             }
+            value += 1;
         }
-
-        return ans;
     }
 }
