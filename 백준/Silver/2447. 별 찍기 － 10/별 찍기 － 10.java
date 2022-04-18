b22bdda2ac988a9038f1f3c2209da475a582b790
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-04-18 23:41:07 +0900

    [Silver I] Title: 별 찍기 - 10, Time: 312 ms, Memory: 25260 KB -BaekjoonHub

---
+++ 백준/Silver/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static char[][] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        arr = new char[N][N];
+        star(0, 0, N, false);
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                bw.write(arr[i][j]);
+            }
+            bw.write("\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    static void star(int x, int y, int N, boolean blank) {
+        if (blank) {
+            for (int i = x; i < x + N; i++) {
+                for (int j = y; j < y + N; j++) {
+                    arr[i][j] = ' ';
+                }
+            }
+            return;
+        }
+
+        if (N == 1) {
+            arr[x][y] = '*';
+            return;
+        }
+
+        int size = N / 3;
+        int cnt = 0;
+        for (int i = x; i < x + N; i += size) {
+            for (int j = y; j < y + N; j += size) {
+                cnt++;
+                if (cnt == 5) {
+                    star(i, j, size, true);
+                }
+                else {
+                    star(i, j, size, false);
+                }
+            }
+        }
+    }
+}
