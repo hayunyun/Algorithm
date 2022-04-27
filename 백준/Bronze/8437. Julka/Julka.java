d7d9e2e5ad91c7d11823cd350c3fa4889c540e1f
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-04-27 22:52:52 +0900

    [Bronze V] Title: Julka, Time: 124 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Bronze/8437. Julka/Julka.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BigInteger total = new BigInteger(bf.readLine());
+        BigInteger diff = new BigInteger(bf.readLine());
+        BigInteger two = new BigInteger("2");
+        
+        BigInteger klaudia = (total.subtract(diff)).divide(two).add(diff); //(total-diff)/2+diff
+        BigInteger natalia = (total.subtract(diff)).divide(two); //(total-diff)/2
+        
+        System.out.println(klaudia);
+        System.out.println(natalia);
+    }
+}
