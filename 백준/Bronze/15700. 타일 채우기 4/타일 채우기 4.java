219dc4d4e226b50ae50f9457be74bc3433953dbf
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-06-18 18:31:06 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 208 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        Long a = sc.nextLong();
+        Long b = sc.nextLong();
+        
+        System.out.println(a * b / 2);
+    }
+}
