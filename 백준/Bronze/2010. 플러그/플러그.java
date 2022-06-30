80bd159c37d65284babfb1c4c641f29168b0faaa
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-06-30 17:16:42 +0900

    [Bronze III] Title: 플러그, Time: 1240 ms, Memory: 308696 KB -BaekjoonHub

---
+++ 백준/Bronze/2010. 플러그/플러그.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += sc.nextInt();
+        }
+        System.out.println(sum - n + 1);
+    }
+}
