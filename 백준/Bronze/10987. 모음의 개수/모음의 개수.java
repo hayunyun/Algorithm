aae4d9914903d8083888d1b4f74ba8e72b60500d
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-07-02 00:29:12 +0900

    [Bronze II] Title: 모음의 개수, Time: 132 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int cnt = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
