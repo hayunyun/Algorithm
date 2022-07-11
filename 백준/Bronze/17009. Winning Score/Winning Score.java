8e0b685b6f9c9b56894df3dcbc6037a774a83441
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-07-11 22:22:29 +0900

    [Bronze IV] Title: Winning Score, Time: 208 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/17009. Winning Score/Winning Score.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+import java.text.SimpleDateFormat;
+ 
+ 
+class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);            
+       int sum=0;
+       int cnt=3;
+       int sum1=0;
+       for(int i=1;i<7;i++) {
+           int a=sc.nextInt();
+           sum+= a*cnt;
+           cnt--;
+           if(i==3) {
+               sum1=sum;
+               cnt=3;
+               sum=0;
+       }      
+    }
+       if(sum1==sum) {
+           System.out.println("T");
+       }
+       else {
+           System.out.println(sum1>sum?"A":"B");
+       }
+}
+}
