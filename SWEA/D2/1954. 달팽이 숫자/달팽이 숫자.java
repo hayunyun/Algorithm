a8144e6a0004cdfd878a7881812c95d7a5a7a307
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-08-02 20:19:08 +0900

    [D2] Title: 달팽이 숫자, Time: 153 ms, Memory: 21,052 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
-
+ 
 public class Solution {
     static int[][] map;
     static int n;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+ 
         int T;
         T=sc.nextInt();
-
-
+ 
+ 
         for(int test_case = 1; test_case <= T; test_case++) {
             n = sc.nextInt();
-
+ 
             map = new int[n][n];
             loop(0, n, 1);
-
+ 
             // Output
             System.out.printf("#%d\n", test_case);
             for (int i = 0; i < n; i++) {
@@ -26,36 +26,32 @@ public class Solution {
             }
         }
     }
-
+ 
     static void loop(int start, int end, int val) {
         if (val == n * n + 1) return;
-        if (start == end) {
-            map[start][start] = val;
-            return;
-        }
-
+ 
         // 우측으로 이동하며 값 증가
         for (int i = start; i < end; i++) {
             map[start][i] = val++;
         }
-
+ 
         // 우측으로 내려가며 값 증가
         for (int i = start + 1; i < end; i++) {
             map[i][end - 1] = val++;
         }
-
+ 
         // 좌측으로 이동하며 값 증가
         for (int i = end - 2; i >= start; i--) {
             map[end - 1][i] = val++;
         }
-
+ 
         // 상단으로 올라가며 값 증가
         for (int i = end - 2; i > start; i--) {
             map[i][start] = val++;
         }
-
+ 
         // 시작값++, 종료값-- 후 내부 사각형 재귀
         loop(start + 1, end - 1, val);
     }
-
+ 
 }
