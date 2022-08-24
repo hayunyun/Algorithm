05a1591ab66be5d27aa794d1c6d0906add2f6213
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-08-24 23:19:13 +0900

    [Gold V] Title: 토마토, Time: 640 ms, Memory: 120844 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,67 +1,68 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[] dx = {0, 0, -1, 1}; // 좌우
-    static int[] dy = {1, -1, 0, 0}; // 상하
-    static int[][] map;
-    static int n, m, cnt;
-    static Queue<int[]> q = new LinkedList<>();
-
-    public static void main(String[] args) throws IOException {
+	static int n, m, days;
+	static int[][] map;
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
+	static Queue<int[]> q;
+	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        cnt = 0;
-
-        map = new int[m][n];
-
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-                if (map[i][j] == 1) q.add(new int[] {i, j});
-            }
+        n = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
+        q = new LinkedList<>();
+        
+        for (int i = 0; i < n; i++) {
+    	   st = new StringTokenizer(br.readLine());
+    	   for (int j = 0; j < m; j++) {
+    		   map[i][j] = Integer.parseInt(st.nextToken());
+    		   if (map[i][j] == 1) q.add(new int[] {i, j});
+    	   }
         }
+        
+        days = 0;
+        BFS();
+	}
+	
+	static void BFS() {
+		while (!q.isEmpty()) {
+			for (int j = q.size() - 1; j >= 0; j--) {
+				int[] now = q.poll();
 
-        bfs();
-        check();
-    }
+				for (int i = 0; i < 4; i++) {
+					int nr = now[0] + dr[i];
+					int nc = now[1] + dc[i];
+					
+					if (nr >= 0 && nr < n && nc >= 0 && nc < m) {
+						if (map[nr][nc] == 0) {
+							map[nr][nc] = 1;
+							q.add(new int[] {nr, nc});
+						}
+					}
+				}				
+			}
+			days++;
+		}
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (map[i][j] == 0) {
+					System.out.println(-1);
+					return;
+				}
+			}
+		}
+		
+		System.out.println(days - 1);
+	}
 
-    static void check() {
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (map[i][j] == 0) {
-                    System.out.println(-1);
-                    return;
-                }
-                else if (map[i][j] > max) max = map[i][j];
-            }
-        }
-        System.out.println(max - 1);
-    }
-
-    static void bfs() {
-        while (!q.isEmpty()) {
-            int[] now = q.poll();
-            int nowX = now[0];
-            int nowY = now[1];
-
-            for (int i = 0; i < 4; i++) {
-                int nextX = nowX + dx[i];
-                int nextY = nowY + dy[i];
-
-                if (nextX >= 0 && nextX < m && nextY >= 0 && nextY < n) {
-                    if (map[nextX][nextY] == 0) {
-                        q.add(new int[] {nextX, nextY});
-                        map[nextX][nextY] = map[nowX][nowY] + 1;
-                    }
-                }
-            }
-        }
-    }
 }
