1e92d53f24d528ab3ea6d4387922c1d748d670df
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-08-28 23:35:14 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 4, Time: 1036 ms, Memory: 89476 KB -BaekjoonHub

---
+++ 백준/Silver/24482. 알고리즘 수업 － 깊이 우선 탐색 4/알고리즘 수업 － 깊이 우선 탐색 4.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static ArrayList<Integer>[] graph;
+    static int[] vis;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList[n + 1];
+        vis = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        for (int i = 1; i <= n; i++) {
+            Collections.sort(graph[i], Collections.reverseOrder());
+        }
+
+        vis[r] = 1;
+
+        dfs(r);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            sb.append(vis[i] - 1).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static void dfs(int node) {
+        for (int i = 0; i < graph[node].size(); i++) {
+            int next = graph[node].get(i);
+            if (vis[next] == 0) {
+                vis[next] = vis[node] + 1;
+                dfs(next);
+            }
+        }
+    }
+}
