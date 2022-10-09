6392f1befa4e3794a23b2c4018ec56ae25868314
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-10-09 19:03:15 +0900

    [Silver I] Title: 카드 구매하기, Time: 100 ms, Memory: 11904 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n;
+    static int[] cards;
+    static int[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+        cards = new int[n + 1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            cards[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        dp = new int[n+1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.max(dp[i], dp[i-j] + cards[j]);
+            }
+        }
+        System.out.println(dp[n]);
+    }
+
+}
