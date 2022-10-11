c44e0e96eed4d39a268d0e74e4125c4beceaba6a
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-10-11 23:09:31 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 764 ms, Memory: 43856 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.java
@@ -5,62 +5,58 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int n, m;
-	static int[][] map, ans;
-	// 시계방향으로 돌며 한칸씩 땡기기 (반대방향)
-	static int[] dr = {0, 1, 0, -1};
-	static int[] dc = {1, 0, -1, 0}; 
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		int r = Integer.parseInt(st.nextToken());
-		map = new int[n][m];
-		
-		int min = Math.min(n, m) / 2;
-		
-		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < m; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
-			}
-		}
-		
-		// r번 회전
-		for (int i = 0; i < r; i++) {
-			// 돌리는 횟수 : 큰거, 작은거 중에 작은거 / 2
-			for (int j = 0; j < min; j++) {
-				int dir = 0;
-				int row = j, col = j;
-				int tmp = map[row][col];
-				
-				while(dir < 4) {
-					int nRow = row + dr[dir];
-					int nCol = col + dc[dir];
-					
-					if (nRow >= j && nRow < n - j && nCol >= j && nCol < m - j) {
-						map[row][col] = map[nRow][nCol];
-						row = nRow;
-						col = nCol;
-					}
-					else dir++;
-				}
-				map[j+1][j] = tmp;					
-			}
-		}
-		
-		
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				sb.append(map[i][j] + " ");
-			}
-			sb.append("\n");
-		}
-		
-		System.out.println(sb);
-	}
+    static int n, m, r;
+    static int[][] map;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken()); // r번 회전
+
+        map = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        turn(r);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                sb.append(map[i][j] + " ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    // 반시계 방향 회전 -> 시계방향으로 구현
+    static int[] dr = {0, 1, 0, -1};
+    static int[] dc = {1, 0, -1, 0};
+    private static void turn(int round) {
+        int cnt = Math.min(m, n) / 2;
+        for (int i = 0; i < round; i++) {
+            for (int j = 0; j < cnt; j++) {
+                int dir = 0;
+                int r = j, c = j;
+                int tmp = map[r][c];
+                while (dir < 4) {
+                    int nr = r + dr[dir];
+                    int nc = c + dc[dir];
+
+                    if (nr >= j && nr < n - j && nc >= j && nc < m - j) {
+                        map[r][c] = map[nr][nc];
+                        r = nr;
+                        c = nc;
+                    } else {
+                        dir++;
+                    }
+                }
+                map[r+1][c] = tmp;
+            }
+        }
+    }
 }
