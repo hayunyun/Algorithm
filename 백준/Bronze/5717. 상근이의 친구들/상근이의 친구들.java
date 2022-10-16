9cccd5d2bc9122d4e97f9bd47aaed2aa5495cd2e
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-10-17 00:31:46 +0900

    [Bronze IV] Title: 상근이의 친구들, Time: 76 ms, Memory: 11324 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            if (n==0 && m == 0) break;
+            sb.append(m+n).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+}
