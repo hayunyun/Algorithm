93147e54c725c27980ee87339fb5723b8e32b952
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-10-22 02:41:21 +0900

    [Gold IV] Title: ⚾, Time: 524 ms, Memory: 61568 KB -BaekjoonHub

--- 백준/Gold/17281. ⚾/⚾.java
+++ 백준/Gold/17281. ⚾/⚾.java
@@ -2,36 +2,40 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] info;
-    static int[] order = new int[9];
-    static boolean[] vis = new boolean[9];
-    static int n, ans;
+    /*
+     * 큐 말고 리스트로 구현해보기
+     */
+    static int n, maxScore;
+    static int[] order;
+    static int[][] scores;
+    static boolean[] vis;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         n = Integer.parseInt(br.readLine());
-        info = new int[n][9];
+        scores = new int[n][9];
         StringTokenizer st;
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 9; j++) {
-                info[i][j] = Integer.parseInt(st.nextToken());
+                scores[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        ans = Integer.MIN_VALUE;
+        vis = new boolean[9];
+        order = new int[9];
+        order[3] = 0;
+        vis[0] = true;
+        maxScore = Integer.MIN_VALUE;
         permutation(0);
-
-        System.out.println(ans);
+        System.out.println(maxScore);
     }
 
     static void permutation(int cnt) {
-        if (cnt > 3 && order[3] != 0) {
-            return;
+        if (cnt == 3) {
+            cnt++;
         }
 
         if (cnt == 9) {
@@ -49,77 +53,108 @@ public class Main {
         }
     }
 
-    /*
-    1. 순열로 선수 순서 정함 (단, 4번째는 0번)
-    2. 이닝 수만큼 게임 진행
-    2-1. 첫번째 순서의 선수부터 돌면서 게임 진행
-    2-2. 1,2,3,4 에 따라 다르게 점수 산출, 도루 표시
-    2-3. 아웃 3번 나올때까지 진행, 3번 나오면 이닝 종료
-     */
-    static void game() {
-        int score = 0, now = 0;
+    private static void game() {
+        // 1루, 2루, 3루 표시
+        boolean[] pos;
+        int score = 0;
+		int now = 0;
         int out;
-        boolean[] doru; // 0: 도루, 1: 1루, 2: 2루, 3: 3루
+//        Queue<Integer> q = new LinkedList<>();
+//        for (int i = 0; i < 9; i++) {
+//            q.add(order[i]);
+//        }
 
+        // n회 이닝 진행
         for (int i = 0; i < n; i++) {
-            doru = new boolean[4];
+            // 3아웃일때까지 한 라운드가 계속된다
+            pos = new boolean[4];
             out = 0;
-            while (true) {
-                int ball = info[i][order[now]];
-                switch (ball) {
-                    case 1:
-                        for (int j = 3; j >= 1; j--) {
-                            if (doru[j]) {
-                                if (j == 3) {
-                                    doru[3] = false;
-                                    score++;
-                                } else {
-                                    doru[j] = false;
-                                    doru[j+1] = true;
-                                }
-                            }
+            while (out < 3) {
+				int ball = scores[i][order[now]];
+//                int now = q.poll();
+
+				switch(ball) {
+                    case 1: // 안타 - 한 루씩 진루
+                        if (pos[3]) {
+                            score++;
+                            pos[3] = false;
                         }
-                        doru[1] = true;
+
+                        if (pos[2]) {
+                            pos[2] = false;
+                            pos[3] = true;
+                        }
+
+                        if (pos[1]) {
+                            pos[1] = false;
+                            pos[2] = true;
+                        }
+
+                        pos[1] = true;
                         break;
-                    case 2:
-                        for (int j = 3; j >= 2; j--) {
-                            if (doru[j]) {
-                                score++;
-                                doru[j] = false;
-                            }
+                    case 2: // 2루타 - 3,2루 점수얻고 1루는 3루로, 타자는 2루로
+                        if (pos[3]) {
+                            score++;
+                            pos[3] = false;
                         }
-                        if (doru[1]) {
-                            doru[1] = false;
-                            doru[3] = true;
+
+                        if (pos[2]) {
+                            score++;
+                            pos[2] = false;
                         }
-                        doru[2] = true;
+
+                        if (pos[1]) {
+                            pos[1] = false;
+                            pos[3] = true;
+                        }
+
+                        pos[2] = true;
                         break;
-                    case 3:
-                        for (int j = 3; j >= 1; j--) {
-                            if (doru[j]) {
-                                score++;
-                                doru[j] = false;
-                            }
+                    case 3: // 3루타 - 3, 2, 1루 점수얻고, 타자는 3루로
+                        if (pos[3]) {
+                            pos[3] = false;
+                            score++;
                         }
-                        doru[3] = true;
+
+                        if (pos[2]) {
+                            pos[2] = false;
+                            score++;
+                        }
+
+                        if (pos[1]) {
+                            pos[1] = false;
+                            score++;
+                        }
+
+                        pos[3] = true;
                         break;
-                    case 4:
-                        for (int j = 3; j >= 1; j--) {
-                            if (doru[j]) {
-                                score++;
-                                doru[j] = false;
-                            }
+                    case 4: // 홈런
+                        if (pos[3]) {
+                            pos[3] = false;
+                            score++;
                         }
+
+                        if (pos[2]) {
+                            pos[2] = false;
+                            score++;
+                        }
+
+                        if (pos[1]) {
+                            pos[1] = false;
+                            score++;
+                        }
+
                         score++;
                         break;
-                    case 0:
+                    case 0: // 아웃
                         out++;
                         break;
                 }
                 now = (now + 1) % 9;
-                if (out == 3) break;
+//                q.add(now);
             }
         }
-        ans = Math.max(ans, score);
+
+        maxScore = Math.max(maxScore, score);
     }
 }
