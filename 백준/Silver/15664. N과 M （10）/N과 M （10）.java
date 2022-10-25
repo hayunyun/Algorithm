ea3ef9871564efc69ed323ab06b8dc3923875bc7
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-10-25 22:47:38 +0900

    [Silver II] Title: N과 M (10), Time: 76 ms, Memory: 11584 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m;
+    static int[] nums, sel;
+    static boolean[] vis;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        nums = new int[n];
+        vis = new boolean[n];
+        sel = new int[m];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+        select(0, 0);
+        System.out.println(sb);
+    }
+
+    static void select(int idx, int cnt) {
+        if (cnt == m) {
+            for (int x : sel) {
+                sb.append(x + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int last = 0; // 이전에 뽑았던 값 저장
+        for (int i = idx; i < n; i++) {
+            if (!vis[i] && last != nums[i]) { // 뽑았던 값이 아니면
+                vis[i] = true;
+                sel[cnt] = nums[i];
+                last = nums[i];
+                select(i + 1, cnt + 1);
+                vis[i] = false;
+            }
+        }
+    }
+}
