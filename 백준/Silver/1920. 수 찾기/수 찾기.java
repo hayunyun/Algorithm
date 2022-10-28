14f4524e1fceae0e7e48af0d5ec458e5a5715445
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-10-28 23:59:44 +0900

    [Silver IV] Title: 수 찾기, Time: 564 ms, Memory: 57112 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,14 +1,19 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int n;
+    static int[] nums;
     static StringBuilder sb = new StringBuilder();
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
-    static StringTokenizer st;
     public static void main(String[] args) throws IOException {
-        int n = Integer.parseInt(br.readLine());
-        int[] nums = new int[n];
-        st = new StringTokenizer(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        nums = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             nums[i] = Integer.parseInt(st.nextToken());
         }
@@ -18,27 +23,27 @@ public class Main {
         int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
-            int x = search(Integer.parseInt(st.nextToken()), nums);
-            sb.append(x).append("\n");
+            sb.append(binarySearch(Integer.parseInt(st.nextToken()))).append("\n");
         }
 
-        System.out.println(sb);  
-        
+        System.out.println(sb);
     }
 
-    public static int search(int m, int[] nums) throws IOException {
+    static int binarySearch(int num) {
         int low = 0;
-        int high = nums.length - 1;
+        int high = n-1;
+        int mid;
         while (low <= high) {
-            int mid = (low + high) / 2;
-            if (m < nums[mid]) {
+            mid = (low + high) / 2;
+            if (num < nums[mid]) {
                 high = mid - 1;
-            }
-            else if (m > nums[mid]) {
+            } else if (num > nums[mid]) {
                 low = mid + 1;
+            } else {
+                return 1;
             }
-            else return 1;
         }
+
         return 0;
     }
 }
