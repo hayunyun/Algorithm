3aaa978981a2b7e77defcb19602eae923574e836
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-11-06 00:12:40 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/lv3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,9 +1,27 @@
--- 코드를 입력하세요
-# 차집합
-SELECT ins.name, ins.datetime from animal_ins ins 
+select i.name, i.datetime from animal_ins i
 left join
-animal_outs outs
-on ins.animal_id = outs.animal_id
-where outs.animal_id is null
-order by datetime limit 3
-# using(animal_id)
+(select * from animal_outs) o
+using(animal_id)
+where o.animal_type is null
+order by datetime
+limit 3
+
+
+
+
+
+
+
+
+
+
+
+# -- 코드를 입력하세요
+# # 차집합
+# SELECT ins.name, ins.datetime from animal_ins ins 
+# left join
+# animal_outs outs
+# on ins.animal_id = outs.animal_id
+# where outs.animal_id is null
+# order by datetime limit 3
+# # using(animal_id)
