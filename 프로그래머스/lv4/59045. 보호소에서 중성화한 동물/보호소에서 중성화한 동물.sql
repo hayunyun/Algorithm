75657f6ab3bf5443569016864648844ae8f89efb
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-11-06 01:14:31 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -1,9 +1,26 @@
--- 코드를 입력하세요
-SELECT 
-ins.animal_id, ins.animal_type, ins.name
-from animal_ins ins
-join
-animal_outs outs
-on (ins.animal_id = outs.animal_id)
-where ins.sex_upon_intake != outs.sex_upon_outcome
+select i.animal_id, i.animal_type, i.name
+from animal_ins i
+join animal_outs o
+using(animal_id)
+where i.sex_upon_intake != o.sex_upon_outcome
 order by animal_id
+
+
+
+
+
+
+
+
+
+
+
+# -- 코드를 입력하세요
+# SELECT 
+# ins.animal_id, ins.animal_type, ins.name
+# from animal_ins ins
+# join
+# animal_outs outs
+# on (ins.animal_id = outs.animal_id)
+# where ins.sex_upon_intake != outs.sex_upon_outcome
+# order by animal_id
