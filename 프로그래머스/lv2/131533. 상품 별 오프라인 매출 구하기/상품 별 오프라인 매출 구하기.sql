2adb2b231cecebb3a4a8014df1e26f0066009ea2
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-11-06 00:02:41 +0900

    [level 2] Title: 상품 별 오프라인 매출 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/131533. 상품 별 오프라인 매출 구하기/상품 별 오프라인 매출 구하기.sql
+++ 프로그래머스/lv2/131533. 상품 별 오프라인 매출 구하기/상품 별 오프라인 매출 구하기.sql
@@ -1,8 +1,29 @@
--- 코드를 입력하세요
-SELECT product_code, (a.price * b.amount) sales from product a
+select product_code, (price * total) sales 
+from product p
 join
-(select product_id, sum(sales_amount) amount from offline_sale
-group by product_id) b
+(select product_id, sum(sales_amount) total from offline_sale group by product_id) o
 using(product_id)
-group by product_code
+
 order by sales desc, product_code
+
+
+
+
+
+
+
+
+
+
+
+# -- 코드를 입력하세요
+# # SELECT product_code, (a.price * b.amount) sales from product a
+# # join
+# # (select product_id, sum(sales_amount) amount from offline_sale
+# # group by product_id) b
+# # using(product_id)
+# # group by product_code
+# # order by sales desc, product_code
+
+# (select product_id, sum(sales_amount) amount from offline_sale
+# group by product_id)
