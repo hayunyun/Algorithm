3e4afe906cfe57c116f0407eb3ba8389ff96c9c6
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-11-06 01:40:33 +0900

    [level 5] Title: 상품을 구매한 회원 비율 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
+++ 프로그래머스/lv5/131534. 상품을 구매한 회원 비율 구하기/상품을 구매한 회원 비율 구하기.sql
@@ -1,8 +1,42 @@
--- 코드를 입력하세요
+# select user_id from user_info
+# where year(joined) = 2021
+
+## 2021년에 가입
+
+select year(sales_date) year, month(sales_date) month,
+count(distinct o.user_id) puchased_users,
+round((count(distinct o.user_id) / (select count(*) from user_info where year(joined) = 2021)), 1) puchased_ratio
+from online_sale o
+join user_info i
+using(user_id)
+where year(i.joined) = 2021
+group by year, month
+order by year, month
+
+# select user_id from user_info
+# where year(joined) = 2021
+
+## 2021년에 가입 && 구매
+# select count(distinct user_id) from online_sale
+# where user_id in (select user_id from user_info
+# where year(joined) = 2021)
+
+
+
+
+
+
+
+
+
+
+
+
+# -- 코드를 입력하세요
 
 # select year(sales_date) year, month(sales_date) month, 
-# count(*) puchased_users, 
-# (round(count(*) / (SELECT count(*) from user_info where year(joined) = 2021), 1)) puchased_ratio
+# count(distinct A.user_id) puchased_users, 
+# (round(count(distinct A.user_id) / (SELECT count(*) from user_info where year(joined) = 2021), 1)) puchased_ratio
 # from online_sale A
 # join
 # (
@@ -13,16 +47,16 @@
 # order by year, month
 
 
-select year(sales_date) year, month(sales_date) month, 
-count(distinct sale.user_id) puchased_users,
-(round(count(distinct sale.user_id) / (SELECT count(user_id) from user_info where year(joined) = 2021), 1)) puchased_ratio
-from online_sale sale
-join user_info info
-on sale.user_id = info.user_id
-where year(info.joined) = 2021
-group by year, month
-order by year, month
+# # select year(sales_date) year, month(sales_date) month, 
+# # count(distinct sale.user_id) puchased_users,
+# # (round(count(distinct sale.user_id) / (SELECT count(user_id) from user_info where year(joined) = 2021), 1)) puchased_ratio
+# # from online_sale sale
+# # join user_info info
+# # on sale.user_id = info.user_id
+# # where year(info.joined) = 2021
+# # group by year, month
+# # order by year, month
 
 
-# SELECT count(*) from user_info where year(joined) = 2021
-# 2021년에 가입한 전체 회원 수
+# # SELECT count(*) from user_info where year(joined) = 2021
+# # 2021년에 가입한 전체 회원 수
