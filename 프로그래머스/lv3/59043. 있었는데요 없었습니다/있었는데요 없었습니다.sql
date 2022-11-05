28473e53a409497389273374301031d5a3ecab01
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-11-06 00:16:53 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,7 +1,22 @@
--- 코드를 입력하세요
-SELECT ins.animal_id, ins.name from animal_ins ins
+select i.animal_id, i.name from animal_ins i
 join
-(select animal_id, datetime from animal_outs) outs
-on ins.animal_id = outs.animal_id
-where ins.datetime > outs.datetime
-order by ins.datetime
+(select * from animal_outs) o
+using(animal_id)
+where o.datetime < i.datetime
+order by i.datetime
+
+
+
+
+
+
+
+
+
+# -- 코드를 입력하세요
+# SELECT ins.animal_id, ins.name from animal_ins ins
+# join
+# (select animal_id, datetime from animal_outs) outs
+# using(animal_id)
+# where ins.datetime > outs.datetime
+# order by ins.datetime
