8b0dc405479c66a82a1f9f6b9cdb2f465fe35ea5
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-11-06 00:33:07 +0900

    [level 4] Title: 5월 식품들의 총매출 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
+++ 프로그래머스/lv4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
@@ -1,9 +1,32 @@
--- 코드를 입력하세요
-SELECT pro.product_id, pro.product_name, (pro.price * ord.amount) total_sales
-from food_product pro
-join
-(select product_id, sum(amount) amount, produce_date from food_order
-where year(produce_date) = 2022 and month(produce_date) = 5
-group by product_id) ord
+select ord.product_id, pro.product_name, sum(amount)*pro.price total_sales
+from food_order ord
+join food_product pro
 using(product_id)
+where year(produce_date) = 2022 and month(produce_date) = 5
+group by product_id
 order by total_sales desc, product_id
+
+
+
+
+
+
+
+
+
+
+
+
+# -- 코드를 입력하세요
+# SELECT pro.product_id, pro.product_name, (pro.price * ord.amount) total_sales
+# from food_product pro
+# join
+# (select product_id, sum(amount) amount, produce_date from food_order
+# where year(produce_date) = 2022 and month(produce_date) = 5
+# group by product_id) ord
+# using(product_id)
+# order by total_sales desc, product_id
+
+# # select product_id, sum(amount) amount, produce_date from food_order
+# # where year(produce_date) = 2022 and month(produce_date) = 5
+# # group by product_id
