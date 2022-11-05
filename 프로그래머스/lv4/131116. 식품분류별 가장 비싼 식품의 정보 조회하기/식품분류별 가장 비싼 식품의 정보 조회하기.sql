29280d6e98774e09fd4673e3c084aca0caba662a
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-11-06 01:55:48 +0900

    [level 4] Title: 식품분류별 가장 비싼 식품의 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
+++ 프로그래머스/lv4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
@@ -1,10 +1,19 @@
 -- 코드를 입력하세요
-SELECT A.category, b.max_price, A.product_name from food_product A,
-(select max(price) max_price, category from food_product 
+select a.category, b.max_price, a.product_name
+from food_product a
+join (select category, max(price) max_price from food_product 
 where category in ('과자', '국', '김치', '식용유')
-group by category) B
-where A.price = B.max_price and A.category = B.category
-order by price desc
+group by category) b
+using(category)
+where a.price = b.max_price
+order by max_price desc
+
+# SELECT A.category, b.max_price, A.product_name from food_product A,
+# (select max(price) max_price, category from food_product 
+# where category in ('과자', '국', '김치', '식용유')
+# group by category) B
+# where A.price = B.max_price and A.category = B.category
+# order by price desc
 
 # select max(price) max_price, category from food_product 
 # where category in ('과자', '국', '김치', '식용유')
