562d098635248a9d774e93762ccb09231a395ef8
hayunyun <50168998+hayunyun@users.noreply.github.com>
2022-11-07 21:43:34 +0900

    [Gold IV] Title: 집합의 표현, Time: 372 ms, Memory: 55284 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -5,64 +5,69 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, m;
-    static int[] parent, rank;
-    public static void main(String[] args) throws IOException {
+	static int n;
+	static int[] parent, rank;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        parent = new int[n+1]; // 0 ~ n
+        int m = Integer.parseInt(st.nextToken());
+        
         rank = new int[n+1];
+        parent = new int[n+1];
         makeSet();
-
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int met = Integer.parseInt(st.nextToken());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-
-            if (met == 0) {
-                union(a,b);
-            } else {
-                if (findSet(a) == findSet(b)) {
-                    sb.append("YES");
-                } else sb.append("NO");
-                sb.append("\n");
-            }
+        
+        for (int tc = 1; tc <= m; tc++) {
+        	st = new StringTokenizer(br.readLine());
+        	int x = Integer.parseInt(st.nextToken());
+        	int a = Integer.parseInt(st.nextToken());
+        	int b = Integer.parseInt(st.nextToken());
+        	
+        	calc(x, a, b);
         }
+        
         System.out.println(sb);
+	}
+	
+	private static void calc(int x, int a, int b) {
+		if (x == 0) {
+			union(a, b);
+		} else {
+			if (findSet(a) == findSet(b)) {
+				sb.append("YES");
+			} else {
+				sb.append("NO");
+			}
+			sb.append("\n");
+		}
+	}
 
-    }
-
-    private static void union(int a, int b) {
-        int pa = findSet(a);
-        int pb = findSet(b);
-
-        if (pa == pb) return;
-
-        if (rank[pa] > rank[pb]) {
-            parent[pb] = pa;
-        } else {
-            parent[pa] = pb;
-            if (rank[pa] == rank[pb]) {
-                rank[pa]++;
-            }
-        }
-    }
-
-    private static int findSet(int a) {
-        if (parent[a] == a) return a;
-
-        return parent[a] = findSet(parent[a]);
-    }
-
-    private static void makeSet() {
-        for (int i = 0; i <= n; i++) {
-            parent[i] = i;
-        }
-    }
+	static void makeSet() {
+		for (int i = 0; i <= n; i++) {
+			parent[i] = i;
+		}
+	}
+	
+	static int findSet(int a) {
+		if (parent[a] == a) return a;
+		
+		return parent[a] = findSet(parent[a]);
+	}
+	
+	static void union(int a, int b) {
+		int pa = findSet(a);
+		int pb = findSet(b);
+		
+		if (pa == pb) return;
+		
+		if (rank[pa] > rank[pb]) {
+			parent[pb] = pa;
+		} else {
+			parent[pa] = pb;
+			if (rank[pa] == rank[pb]) {
+				rank[pa]++;
+			}
+		}
+	}
 }
